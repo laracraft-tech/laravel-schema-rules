ff05b2c97faf8c5544db7b02aa271d950726f583
Sairahcaz <zacharias.creutznacher@gmail.com>
2023-06-14 21:30:21 +0200

    check table and columns - missing files

---
+++ src/Exceptions/ColumnDoesNotExistException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace LaracraftTech\LaravelSchemaRules\Exceptions;
+
+use Exception;
+
+class ColumnDoesNotExistException extends Exception
+{
+
+}
---
+++ src/Exceptions/TableDoesNotExistException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace LaracraftTech\LaravelSchemaRules\Exceptions;
+
+use Exception;
+
+class TableDoesNotExistException extends Exception
+{
+
+}
