9290ce63d063ee8e8e408a7a902abcaca8bea38b
Sairahcaz <zacharias.creutznacher@gmail.com>
2023-06-18 15:58:16 +0200

    remove refresh database usage from test

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,8 +1,7 @@
 <?php
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use LaracraftTech\LaravelSchemaRules\Tests\TestCase;
 
-uses(TestCase::class, RefreshDatabase::class)->beforeEach(function () {
+uses(TestCase::class)->beforeEach(function () {
     ray()->clearAll();
 })->in(__DIR__);
